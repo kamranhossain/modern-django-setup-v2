5d0d5ec00accfb275fd8243e3b7cda78430f8f82
kamranhossain <kamran.hossain227@gmail.com>
2020-04-20 22:03:55 +0600

    add api model

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+
+class Api(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        get_user_model(), null=True, on_delete=models.CASCADE
+    )
