04a515b2c843cd312e37279de729cb3d6d3db884
kamranhossain <kamran.hossain227@gmail.com>
2020-04-20 21:41:18 +0600

    delete serializers and urls for app and clear views

--- apps/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path, include
-from rest_framework import routers
-from apps.api import views
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-
-urlpatterns = [
-    re_path(r'^', include(router.urls)),
-]
